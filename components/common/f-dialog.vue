8c80bdb7d2b30fe60246003794090b64e17921ef
luosiyu <1514813390@qq.com>
2020-02-09 21:27:28 +0800

    add create

---
+++ components/common/f-dialog.vue
@@ -0,0 +1,68 @@
+<template>
+	<uni-popup ref="dialog">
+		<view style="width: 600rpx;" class="bg-white rounded">
+			<view class="flex align-center justify-center font-weight-bold border-bottom border-light-secondary" style="height: 100rpx;">{{title}}</view>
+			<view class="flex align-center justify-center p-3">
+				<slot></slot>
+			</view>
+			<view class="flex border-top border-light-secondary"
+			style="height: 100rpx;">
+				<view class="flex-1 text-muted flex align-center justify-center"
+				@tap="cancel">{{cancelText}}</view>
+				<view class="flex-1 text-main flex align-center justify-center"
+				@tap="confirm">{{confirmText}}</view>
+			</view>
+		</view>
+	</uni-popup>
+</template>
+
+<script>
+	import uniPopup from '@/components/uni-ui/uni-popup/uni-popup.vue';
+	export default {
+		components: {
+			uniPopup
+		},
+		props: {
+			title: {
+				type: String,
+				default: "提示"
+			},
+			cancelText:{
+				type: String,
+				default: "取消"
+			},
+			confirmText:{
+				type: String,
+				default: "确定"
+			}
+		},
+		data() {
+			return {
+				callback: false
+			}
+		},
+		methods: {
+			open(callback = false){
+				this.callback = callback
+				this.$refs.dialog.open()
+			},
+			cancel() {
+				this.$emit('cancel')
+				this.$refs.dialog.close()
+			},
+			confirm(){
+				if(typeof this.callback === 'function'){
+					this.callback(()=>{
+						this.cancel()
+					})
+				} else {
+					this.$emit('confirm')
+					this.cancel()
+				}
+			}
+		},
+	}
+</script>
+
+<style>
+</style>
---
+++ pages/create/create.vue
@@ -0,0 +1,72 @@
+<template>
+	<view>
+		<view style="height: 350rpx;" class="bg-light position-relative" hover-class="bg-hover-light" @tap="upload">
+			<image v-if="form.cover" :src="form.cover" mode="aspectFill"
+			style="width: 750rpx;height: 350rpx;"></image>
+			<view class="position-absolute left-0 right-0 top-0 bottom-0 flex flex-column align-center justify-center" style="background-color: rgba(255,255,255,0.2);">
+				<text v-if="!form.cover" class="iconfont iconjia" style="font-size: 50rpx;"></text>
+				<text class="font text-muted">点击{{form.cover ? '切换' : '添加'}}封面图</text>
+			</view>
+		</view>
+		<form-item label="标题">
+			<input type="text" v-model="form.title" placeholder="请填写视频标题" placeholder-class="text-light-muted" class="w-100 pr-5"/>
+		</form-item>
+		<picker mode="selector" :range="range" @change="change">
+			<form-item label="分类" rightIcon>
+				<input type="text" v-model="form.category" placeholder="请填写视频标题" placeholder-class="text-light-muted" class="w-100 pr-5" disabled/>
+			</form-item>
+		</picker>
+		<form-item label="描述">
+			<textarea v-model="form.desc" placeholder="请填写视频描述" style="width: 550rpx;" class="py-3"/>
+		</form-item>
+		<!-- #ifdef MP -->
+		<view class="py-2 px-3">
+			<main-big-button @tap="submit">发 布</main-big-button>
+		</view>
+		<!-- #endif -->
+	</view>
+</template>
+
+<script>
+	import formItem from '@/components/common/form-item.vue';
+	import mainBigButton from '@/components/common/main-big-button.vue';
+	export default {
+		components: {
+			formItem,
+			mainBigButton
+		},
+		data() {
+			return {
+				form:{
+					cover:"",
+					title:"",
+					category:"",
+					desc:""
+				},
+				range:["分类1","分类2","分类3"]
+			}
+		},
+		methods: {
+			// 点击添加封面图
+			upload(){
+				uni.chooseImage({
+				    count: 1,
+				    sizeType: ['compressed'],
+				    success: (res)=> {
+				        this.form.cover = res.tempFilePaths[0]
+				    }
+				});
+			},
+			change(e){
+				this.form.category = this.range[e.detail.value]
+			},
+			submit(){
+				console.log('发布');
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
