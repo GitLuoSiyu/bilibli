0549f51b2b084285b677d583f93c8bd31b5835bd
luosiyu <1514813390@qq.com>
2020-02-13 21:59:06 +0800

    add user-fava

--- pages/user-fava/user-fava.vue
+++ pages/user-fava/user-fava.vue
@@ -1,46 +1,57 @@
 <template>
-	<view>
-		<view v-for="(item, index) in list" :key="index">
-			<media-list :item="item" :index="index"></media-list>
-		</view>
+	<view v-for="(item, index) in list" :key="index">
+		<uni-swipe-action>
+			<uni-swipe-action-item :options="options" @click="bindClick">
+				<media-list :item="item" :index="index"></media-list>
+			</uni-swipe-action-item>
+		</uni-swipe-action>
 	</view>
 </template>
 
 <script>
 import mediaList from '@/components/common/media-list.vue';		
-	
+import uniSwipeAction from '@/components/uni-ui/uni-swipe-action/uni-swipe-action.vue';	
+import uniSwipeActionItem from '@/components/uni-ui/uni-swipe-action-item/uni-swipe-action-item.vue';
 export default {
 	components:{
-		mediaList
+		mediaList,uniSwipeAction,uniSwipeActionItem
 	},
 	data() {
 		return {
+			options: [{
+				text: "取消收藏"
+			}],
 			list:[
 				{
+					id:0,
 					cover: '/static/demo/list2/1.jpg',
 					title: 'Uni-app第二季实战商城项目上线了',
 					create_time: '今天06:00',
 					play_count: 0,
 					danmu_count: 0
 				},{
+					id:1,
 					cover: '/static/demo/list2/1.jpg',
 					title: 'Uni-app第二季实战商城项目上线了',
 					create_time: '今天06:00',
 					play_count: 0,
 					danmu_count: 0
 				},{
+					id:2,
 					cover: '/static/demo/list2/1.jpg',
 					title: 'Uni-app第二季实战商城项目上线了',
 					create_time: '今天06:00',
 					play_count: 0,
 					danmu_count: 0
 				},{
+					id:3,
 					cover: '/static/demo/list2/1.jpg',
 					title: 'Uni-app第二季实战商城项目上线了',
 					create_time: '今天06:00',
 					play_count: 0,
 					danmu_count: 0
 				},{
+					id:4,
 					cover: '/static/demo/list2/1.jpg',
 					title: 'Uni-app第二季实战商城项目上线了',
 					create_time: '今天06:00',
@@ -52,6 +63,14 @@ export default {
 	},
 	methods: {
 		
+		// 
+		bindClick(e,item) {
+			console.log(e,item);
+			uni.showToast({
+				title: `点击了${e.content.text}按钮`,
+				icon: "none"
+			})
+		}
 	}
 }
 </script>
