088c5acc367c6baf33be837cda10ae5627c72418
luosiyu <1514813390@qq.com>
2020-05-09 00:08:54 +0800

    update

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -14,6 +14,7 @@
 		<view class="px-4">
 			<input type="text" value="" class="uni-input bg-light rounded mb-4" placeholder="手机号/用户名/邮箱" />
 			<input type="text" value="" class="uni-input bg-light rounded mb-4" placeholder="请输入密码" />
+			<input type="text" value="" class="uni-input bg-light rounded mb-4" placeholder="请输入确认密码" />
 		</view>
 		
 		<view class="px-4">
@@ -24,6 +25,31 @@
 			<view style="height: 2rpx; background-color: #E3E1DD;width: 100rpx;"></view>
 			<view class="text-muted mx-2 font-sm">社交账号登录</view>
 			<view style="height: 2rpx; background-color: #E3E1DD;width: 100rpx;"></view>
+		</view>
+		
+		<view class="flex align-center px-5">
+			<view class="flex justify-center align-center flex-1">
+				<view class="rounded-circle flex justify-center align-center bg-light" style="120rpx; height: 120rpx;">
+					<text class="iconfont iconweixin"></text>
+				</view>
+			</view>
+			<view class="flex justify-center align-center flex-1">
+				<view class="rounded-circle flex justify-center align-center bg-light" style="120rpx; height: 120rpx;">
+					<text class="iconfont iconQQ"></text>
+				</view>
+			</view>
+			<view class="flex justify-center align-center flex-1">
+				<view class="rounded-circle flex justify-center align-center bg-light" style="120rpx; height: 120rpx;">
+					<text class="iconfont iconxinlangweibo"></text>
+				</view>
+			</view>
+		</view>
+		
+		<!-- 注册 -->
+		<view class="flex justify-center align-center py-5">
+			<view class="text-muted mx-2 font-sm">
+				注册账号
+			</view>
 		</view>
 		
 	</view>
