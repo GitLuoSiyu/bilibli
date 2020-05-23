179b0a9ca6f5dfe569b7493fe00511f5531bc2f6
luosiyu <1514813390@qq.com>
2020-05-24 00:24:42 +0800

    add video

--- App.vue
+++ App.vue
@@ -15,13 +15,17 @@
 <style>
 	/*每个页面公共css */
 	/* 引入官方样式库 */
+	/* #ifndef APP-NVUE */
 	@import url("./common/uni.css");
 	/* 引入animate动画库 */
 	@import url("./common/animate.css");
+	/* #endif */
 	/* 引入free样式库 */
 	@import url("./common/free.css");
 	/* 引入全局样式库 */
 	@import url("./common/common.css");
 	/* 引入自定义图标库 */
+	/* #ifndef APP-NVUE */
 	@import url("./common/icon.css");
+	/* #endif */
 </style>
