b35595f363aa32b1d78353036fe357d4ca5b6ee0
luosiyu <1514813390@qq.com>
2020-05-23 23:55:07 +0800

    修复问题

--- components/common/f-popup.vue
+++ components/common/f-popup.vue
@@ -1,27 +1,31 @@
 <template>
-	<view class="position-fixed top-0 left-0 right-0 bottom-0 animated faster fadeIn" style="background-color: rgba(0,0,0,0.3);z-index: 999;" v-if="showToast" @click.stop="hide">
-		<slot></slot>
+	<view>
+		<view v-if="status">
+			<view class="position-fixed top-0 left-0 right-0 bottom-0 animated faster fadeIn" style="background-color: rgba(0,0,0,0.3);z-index: 1000;" @click.stop="hide">
+				<slot></slot>
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
-export default {
-	data() {
-		return{
-			showToast: false
-		}
-	},
-	methods:{
-		show() {
-			this.showToast = true
+	export default {
+		data() {
+			return {
+				status:false
+			}
 		},
-			
-		
-		hide() {
-			this.showToast = false
-		}
+		methods: {
+			// 显示
+			show(){
+				this.status = true
+			},
+			// 隐藏
+			hide() {
+				this.status = false
+			}
+		},
 	}
-}
 </script>
 
 <style>
